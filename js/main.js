7ffc81d4ef4af9f598980cb4fe732c153c47d7fb
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-20 11:14:03 -0500

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -17,3 +17,48 @@
   });
 
 })(jQuery);
+
+$(document).ready(function() {
+            $('a[href^="#"]').on('click', function(e) {
+                e.preventDefault();
+
+                var target = this.hash,
+                    $target = $(target);
+
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top
+                }, 1000, 'swing', function() {
+                    window.location.hash = target;
+                });
+            });
+        });
+
+        $(".sidebarBtn").on('click', function() {
+  $(this).toggleClass("on");
+  $('.menu-section').toggleClass("on");
+  $("nav ul").toggleClass('hidden');
+});
+
+// SKILLS
+
+var lang = {
+  "html": "100%",
+  "css": "90%",
+  "javascript": "70%",
+  "php": "55%",
+  "angular": "65%"
+};
+
+var multiply = 4;
+
+$.each( lang, function( language, pourcent) {
+
+  var delay = 700;
+  
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
+  
+  multiply++;
+
+});
